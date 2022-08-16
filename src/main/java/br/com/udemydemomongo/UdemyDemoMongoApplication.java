7c9f978e89a512f09a18207d78313d5dd0494a2d
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-16 14:26:51 -0300

    Initial commit

---
+++ src/main/java/br/com/udemydemomongo/UdemyDemoMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemydemomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyDemoMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyDemoMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/udemydemomongo/UdemyDemoMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemydemomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyDemoMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
