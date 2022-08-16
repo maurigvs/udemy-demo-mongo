22d7582d5a7c2f28af08418ceab31afbf38944a0
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-16 17:41:29 -0300

    Post Comments implementation

--- src/main/java/br/com/udemydemomongo/config/Instantiation.java
+++ src/main/java/br/com/udemydemomongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package br.com.udemydemomongo.config;
 import br.com.udemydemomongo.domain.Post;
 import br.com.udemydemomongo.domain.User;
 import br.com.udemydemomongo.dto.AuthorDTO;
+import br.com.udemydemomongo.dto.CommentDTO;
 import br.com.udemydemomongo.repositories.PostRepository;
 import br.com.udemydemomongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,21 @@ public class Instantiation implements CommandLineRunner {
         Post p2 = new Post(null, sdf.parse("15/08/2022"),
                 "Bom dia!", "Acordei feliz hoje!",
                 new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!",
+                sdf.parse("21/03/2018"),
+                new AuthorDTO(alex));
+
+        CommentDTO c2 = new CommentDTO("Aproveite!",
+                sdf.parse("22/03/2018"),
+                new AuthorDTO(bob));
+
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",
+                sdf.parse("23/03/2018"),
+                new AuthorDTO(alex));
+
+        p1.getComments().addAll(Arrays.asList(c1, c2));
+        p2.addComment(c3);
 
         postRepository.saveAll(Arrays.asList(p1, p2));
 
