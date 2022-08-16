37e5ec67aaaa66c9922a1d6ccc82204b531f3704
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-16 16:14:21 -0300

    User insert and delete methods

--- src/main/java/br/com/udemydemomongo/services/UserService.java
+++ src/main/java/br/com/udemydemomongo/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.udemydemomongo.services;
 
 import br.com.udemydemomongo.domain.User;
+import br.com.udemydemomongo.dto.UserDTO;
 import br.com.udemydemomongo.repositories.UserRepository;
 import br.com.udemydemomongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,18 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("User not found by Id: " + id));
+    }
+
+    public User insert(User user){
+        return repository.save(user);
+    }
+
+    public void delete(String id){
+        User user = findById(id);
+        repository.deleteById(user.getId());
+    }
+
+    public User fromDTO(UserDTO userDto){
+        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
     }
 }
