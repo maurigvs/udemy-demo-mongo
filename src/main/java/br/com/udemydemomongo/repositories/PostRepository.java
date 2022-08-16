118d99d6932d0675b7541fd40a869d5fb3bdc7e6
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-16 18:08:50 -0300

    Posts findByTitle with Query Search

--- src/main/java/br/com/udemydemomongo/repositories/PostRepository.java
+++ src/main/java/br/com/udemydemomongo/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package br.com.udemydemomongo.repositories;
 
 import br.com.udemydemomongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     // Usando o recurso Query Methods
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
